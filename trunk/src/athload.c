3d145df10310e055aef0083425b70b2b747d01d1
jens-chr <jens-chr@c4c806e3-65e6-0310-8aed-90c1e412239e>
2004-11-20 02:36:56 +0000

    enhanced athload; fixed klinux-x86_64

--- trunk/src/athload.c
+++ trunk/src/athload.c
@@ -5,25 +5,70 @@
 
 #define ATHOMUX_BOOTSTRAP
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
 #include "init.h"
 
+char * init = "athload_help";
+char * param = "";
+bool wait_after_init = FALSE;
+
+sig_t old_SIGINT;
+
+
+int do_shutdown() {
+  signal(SIGINT, old_SIGINT);
+
+  if(ATHOMUX_CLEANUP(init, param)) {
+    printf("%s terminated normally.\n", init);
+    return 0;
+  } else {
+    printf("%s terminated with error!\n", init);
+    return 2;
+  }
+}
+
+void shutdown_handler(int signum)
+{
+  printf("Shutdown requested...\n");
+  exit(do_shutdown());
+}
 
 int main(int argc, char * argv[])
 {
+  int arg_nr = 0;
+  int i;
+  for (i=1; i<argc; i++) {
+    if(!arg_nr && (argv[i][0] == '-')) {
+      if(!strcmp(argv[i], "--help"))
+	break;
+      else if(!strcmp(argv[i], "-w") || !strcmp(argv[i], "--wait"))
+	wait_after_init = TRUE;
+      else
+	printf("unknown option '%s'\n", argv[i]);
+    } else {
+      if(++arg_nr == 1)
+	init = argv[i];
+      else if(arg_nr == 2)
+	param = argv[i];
+    }
+  }
+
   ATHOMUX_INIT_INIT();
 
-  if(argc>1) {
-    if(argc>2) {
-      ATHOMUX_INIT(argv[1], argv[2]);
-      ATHOMUX_CLEANUP(argv[1], argv[2]);
-    } else {
-      ATHOMUX_INIT(argv[1], "");
-      ATHOMUX_CLEANUP(argv[1], "");
-    }
-  } else {
-    ATHOMUX_INIT("athload_help", "");
-    ATHOMUX_CLEANUP("athload_help", "");
+  old_SIGINT = signal(SIGINT, shutdown_handler);
+
+  if(!ATHOMUX_INIT(init, param)) {
+    printf("%s terminated initialization with error!\n", init);
+    return 1;
   }
-  printf("%s terminated normally.\n", argv[0]);
-  return 0;
+
+  if(wait_after_init) {
+    while(1) {
+      pause();
+    }
+  }
+
+  return do_shutdown();
 }
