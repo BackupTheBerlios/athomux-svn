c4e2a09af05aa261660415117999a0ccdc6a6660
nieserd <nieserd@c4c806e3-65e6-0310-8aed-90c1e412239e>
2005-11-12 12:33:52 +0000

    Updated comment a little

--- trunk/src/param_scanner.h
+++ trunk/src/param_scanner.h
@@ -14,7 +14,13 @@
 
 
 
-// search in 'param' for option 'name'.
+// search in string 'param' for option 'name'.
+// options are of the form "name[=value]" and separated by whitespaces (space, tab, newline)
+// "name" may consist of any non-whitespace characters.
+// "value" is optional, to enable it to contain spaces, it may be quoted ("" or '').
+// examples for a paramstring:
+// myoption youroption somevalue=12 others="bernie ert grooby" eo_num='unu du tri kvar kven'
+//
 // if this option is found 'found' is set to TRUE, otherwise it is set to FALSE.
 // if this option is found and has a value 'val' and 'val_len' are set accordingly, otherwise they are set to NULL and 0.
 // finds the first match in 'param'.
