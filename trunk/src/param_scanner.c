092929062a4da45b18d6e1c2db58220408029ead
nieserd <nieserd@c4c806e3-65e6-0310-8aed-90c1e412239e>
2006-06-11 21:09:34 +0000

    Added a variation of param_scan (param_check) that allows parameter scanning in one line

--- trunk/src/param_scanner.c
+++ trunk/src/param_scanner.c
@@ -19,15 +19,15 @@ const char param_delimchars[] = "\t\r\n ";
 
 // returns length of prefix of 'start' that contains no character listed in 'toc'
 int excl_len(const char * const start, const char * const toc) {
-  if ((start==NULL) || (toc==NULL)) return 0;
+	if ((start==NULL) || (toc==NULL)) return 0;
 
-  int i=0;
-  while (start[i]!=0) {
-    if (strchr(toc, start[i])!=NULL) break;
-    i++;
-  }
+	int i=0;
+	while (start[i]!=0) {
+		if (strchr(toc, start[i])!=NULL) break;
+		i++;
+	}
 
-  return i;
+	return i;
 }
 
 
@@ -35,15 +35,15 @@ int excl_len(const char * const start, const char * const toc) {
 
 // returns length of prefix of 'start' that contains only characters listed in 'toc'
 int incl_len(const char * const start, const char * const toc) {
-  if ((start==NULL) || (toc==NULL)) return 0;
+	if ((start==NULL) || (toc==NULL)) return 0;
 
-  int i=0;
-  while (start[i]!=0) {
-    if ((strchr(toc, start[i]))==NULL) break;
-    i++;
-  }
+	int i=0;
+	while (start[i]!=0) {
+		if ((strchr(toc, start[i]))==NULL) break;
+		i++;
+	}
 
-  return i;
+	return i;
 }
 
 
@@ -53,77 +53,77 @@ int incl_len(const char * const start, const char * const toc) {
 // if found 'name', 'name_len', 'val' and 'val_len' are set accordingly, otherwise they are set to NULL and 0.
 // the number of bytes read are returned.
 int next_opt(const char * const start, const char ** name, int * name_len, const char ** val, int * val_len) {
-  *name = NULL;
-  *name_len = 0;
-  *val = NULL;
-  *val_len = 0;
+	*name = NULL;
+	*name_len = 0;
+	*val = NULL;
+	*val_len = 0;
 
-  if (start==NULL) return 0;
+	if (start==NULL) return 0;
 
-  const char *p = start;
+	const char *p = start;
 
-  int len;
-  int r_sum = 0;
+	int len;
+	int r_sum = 0;
 
-  len = incl_len(p, param_delimchars);
-  p+=len;
-  r_sum+=len;
+	len = incl_len(p, param_delimchars);
+	p+=len;
+	r_sum+=len;
 
-  // read name
-  *name = p;
-  len = excl_len(p, param_nidchars);
-  p+=len;
-  r_sum+=len;
-  *name_len=len;
+	// read name
+	*name = p;
+	len = excl_len(p, param_nidchars);
+	p+=len;
+	r_sum+=len;
+	*name_len=len;
 
-  len = incl_len(p, param_delimchars);
-  p+=len;
-  r_sum+=len;
+	len = incl_len(p, param_delimchars);
+	p+=len;
+	r_sum+=len;
 
-  if (p[0]=='=') {
-    // name has a value
-    p++;
-    r_sum++;
+	if (p[0]=='=') {
+		// name has a value
+		p++;
+		r_sum++;
 
-    len = incl_len(p, param_delimchars);
-    p+=len;
-    r_sum+=len;
+		len = incl_len(p, param_delimchars);
+		p+=len;
+		r_sum+=len;
 
-    if (p[0]=='"') {
-      // read until second double quote
-      p++;
-      r_sum++;
-      *val = p;
-      len = excl_len(p, "\"");
-      p+=len+1;
-      r_sum+=len+1;
-      *val_len=len;
-    }
-    else if (p[0]=='\'') {
-      // read until second quote
-      p++;
-      r_sum++;
-      *val = p;
-      len = excl_len(p, "'");
-      p+=len+1;
-      r_sum+=len+1;
-      *val_len=len;
-    }
-    else {
-      // no quotes
-      *val = p;
-      len = excl_len(p, param_delimchars);
-      p+=len;
-      r_sum+=len;
-      *val_len=len;
-    }
+		if (p[0]=='"') {
+			// read until second double quote
+			p++;
+			r_sum++;
+			*val = p;
+			len = excl_len(p, "\"");
+			p+=len+1;
+			r_sum+=len+1;
+			*val_len=len;
+		}
+		else if (p[0]=='\'') {
+			// read until second quote
+			p++;
+			r_sum++;
+			*val = p;
+			len = excl_len(p, "'");
+			p+=len+1;
+			r_sum+=len+1;
+			*val_len=len;
+		}
+		else {
+			// no quotes
+			*val = p;
+			len = excl_len(p, param_delimchars);
+			p+=len;
+			r_sum+=len;
+			*val_len=len;
+		}
 
-    len = incl_len(p, param_delimchars);
-    p+=len;
-    r_sum+=len;
-  }
+		len = incl_len(p, param_delimchars);
+		p+=len;
+		r_sum+=len;
+	}
 
-  return r_sum;
+	return r_sum;
 }
 
 
@@ -133,32 +133,69 @@ int next_opt(const char * const start, const char ** name, int * name_len, const
 // If this option is found, 'found' is set to TRUE, otherwise it is set to FALSE.
 // If given 'val' or 'val_len' is NULL, any option value is ignored.
 // If the option has a value, it is returned in 'val', its length in 'val_len', otherwise NULL/0 is returned.
-void param_getopt(const char * const param, const char * const name, bool * found, const char ** val, int * val_len) {
-  if (val) *val = NULL;
-  if (val_len) *val_len = 0;
-  *found = FALSE;
+void param_getopt(const char *const param, const char *const name, bool * found, const char ** val, int * val_len) {
+	if (val) *val = NULL;
+	if (val_len) *val_len = 0;
+	*found = FALSE;
 
-  if (param == NULL || name == NULL) return;
+	if (param == NULL || name == NULL) {
+		*found = FALSE;
+		return;
+	}
 
-  const char *p = param;
-  int len;
+	const char *p = param;
+	int len;
 
-  while (p[0]!=0) {
-    const char * cur_name;
-    int cur_name_len;
-    const char * cur_val;
-    int cur_val_len;
-    
-    len = next_opt(p, &cur_name, &cur_name_len, &cur_val, &cur_val_len);
-    p+=len;
+	while (p[0]!=0) {
+		const char * cur_name;
+		int cur_name_len;
+		const char * cur_val;
+		int cur_val_len;
+		
+		len = next_opt(p, &cur_name, &cur_name_len, &cur_val, &cur_val_len);
+		p+=len;
 
-    if ((cur_name_len>0) && (strncmp(cur_name, name, cur_name_len)==0)) {
-      *found = TRUE;
-      if (cur_val_len>0 && val && val_len) {
-        *val = cur_val;
-        *val_len = cur_val_len;
-      }
-      return;
-    }
-  }
+		if ((cur_name_len>0) && (strncmp(cur_name, name, cur_name_len)==0)) {
+			*found = TRUE;
+			if (cur_val_len>0 && val && val_len) {
+				*val = cur_val;
+				*val_len = cur_val_len;
+			}
+			return;
+		}
+	}
+}
+
+
+bool param_check (const char *const param, const char *const name, const char **val, int *val_len) {
+	if (val) *val = NULL;
+	if (val_len) *val_len = 0;
+	if (param == NULL || name == NULL) {
+		return FALSE;
+	}
+	const char *p = param;
+	int len;
+	while (*p != '\0') {
+		const char *cur_name;
+		int cur_name_len;
+		const char *cur_val;
+		int cur_val_len;
+		len = next_opt(p, &cur_name, &cur_name_len, &cur_val, &cur_val_len);
+		p += len;
+		if ((cur_name_len > 0) && (strncmp(cur_name, name, cur_name_len) == 0)) {
+			if ((cur_val == NULL) != (val == NULL)) {
+				return FALSE;
+			}
+			if (val && val_len) {
+				*val = cur_val;
+				*val_len = cur_val_len;
+				return TRUE;
+			}
+			else if (!val && !val_len) {
+				return TRUE;
+			}
+			return FALSE;
+		}
+	}
+	return FALSE;
 }
--- trunk/src/param_scanner.h
+++ trunk/src/param_scanner.h
@@ -16,19 +16,27 @@
 
 // Searches in string 'param' for option 'name'.
 // Options are of the form "name[=value]" and separated by whitespaces (space, tab, newline).
-// "name" may consist of any non-whitespace characters.
+// "name" may consist of any non-whitespace characters (except "=" and control chars, of course).
 // "value" is optional, to enable it to contain spaces, it may be quoted ("" or '').
 // Examples for a paramstring:
 // myoption youroption somevalue=12 others="bernie ert grooby" eo_num='unu du tri kvar kven'
 //
-// 'param' contains the string to search (usually @param)
+// 'params' contains the string to search (usually @param)
 // 'name' contains the name of the option to be queried
 // In 'found' the result of the search is stored: whether the option was found or not
 // On 'val'!=NULL, a pointer to the value of the option is returned there, NULL if the option has no value.
 // On 'val_len'!=NULL, the length of the value is stored there, 0 if the option has no value.
-void param_getopt(const char * const param, const char * const name, bool * found, const char ** val, int * val_len);
+void param_getopt(const char * const params, const char * const name, bool * found, const char ** val, int * val_len);
 
-
+/* Upper procedure can be cumbersome to call but kept for compatibility. This one is more convenient:
+ * - 'params' contains the whole parameter line (usually "@param")
+ * - 'name' contains the name of the option to be queried
+ * - On 'val' == null, 'val_len' is ignored, and the parameter must not have a value.
+ * - On 'val' != null, the parameter must have a value (written as "name=value").
+ *   *val points to this value afterwards, the length is written to *val_len.
+ * - return value is TRUE iff the parameter 'name' in the correct format (with/wo. value) was found.
+ */
+bool param_check(const char * const params, const char *const name, const char **val, int *val_len);
 
 
 #endif
