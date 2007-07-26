64577ba2bc8207bfd985f8becba9d23eee3c07c2
axh <axh@c4c806e3-65e6-0310-8aed-90c1e412239e>
2007-07-26 19:08:33 +0000

    rename brick.xsl to brick_html.xsl. add namespace in toc xml file. add transforming of toc xml file to html file. integrate toc transformation in transform.pl.

---
+++ trunk/src/doc/xslt/toc_html.xsl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns="http://www.w3.org/1999/xhtml">
+<xsl:output method="xml" indent="yes"
+	doctype-system ="http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"
+	doctype-public="-//W3C//DTD XHTML 1.0 Strict//EN" 
+	omit-xml-declaration="no"
+	encoding="utf-8" />
+	
+	<xsl:template match="/">
+				
+		<html xmlns="http://www.w3.org/1999/xhtml">
+			<head>
+		    		<title>Athomux - Index</title>
+		    		<link rel="stylesheet" type="text/css" href="style.css" />
+			</head>
+			
+			<body>
+				<ul>
+					<xsl:apply-templates select="toc"/>
+				</ul>
+			</body>
+		</html>
+	</xsl:template>
+	
+	
+	<xsl:template match="toc">
+		<xsl:variable name="brickname" select="brickname"/>
+		<li><a href="{$brickname}.html" ><xsl:value-of select="brickname"/></a></li>
+	</xsl:template>
+	
+</xsl:stylesheet>
--- trunk/src/doc/xslt/transform.pl
+++ trunk/src/doc/xslt/transform.pl
@@ -1,28 +1,32 @@
 #!/usr/bin/perl -w
 
 # Transforming bricks from xml to html
-# Creating table of contens
-
-use File::Copy;
+# Creating table of contents
 
 opendir(DIR, ".") or die("couldn't open current directory");
 @files = grep(/\.xml$/,readdir(DIR));
 closedir(DIR);
 
-# load empty table of contens
-copy("empty.toc", "data.toc");
+# load empty table of contents
+system("cp empty.toc data.toc");
+
+print "Transforming...\n";
 
 foreach $file (@files) {
-   
-   print "$file\n";
+
+   print "--> $file\n";
    $filename = substr($file, 0, - 4);
-   
+
    # transform bricks
-   system("saxon $filename.xml brick.xsl > $filename.html"); 
-   
+   system("saxon $filename.xml brick.xsl > $filename.html");
+
    # create table of contents
    system("saxon data.toc toc.xsl title=$file > temp.toc");
-   system("mv temp.toc data.toc"); 
+   system("mv temp.toc data.toc");
 }
 
-print "finished\n";
+# transform table of contents
+print "--> data.toc\n";
+system("saxon data.toc toc_html.xsl > toc.html");
+
+print "...finished\n";
